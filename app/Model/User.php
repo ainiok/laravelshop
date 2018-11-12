e6f1db788507dd9541570fbc595900abcd7770cf
ainiok <job@ainiok.com>
2018-11-12 10:54:08 +0800

    add

--- app/Model/User.php
+++ app/Model/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'created_at', 'updated_at',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
