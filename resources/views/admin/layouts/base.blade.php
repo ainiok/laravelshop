9fa994b3ce66273536b11abaefe14ed89d3ceb97
xiao <jx_xiaojin@aliyun.com>
2018-11-14 23:40:50 +0800

    add

---
+++ resources/views/admin/layouts/base.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>layuiAdmin 内容系统 - 文章列表</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="{{asset('static/admin/layuiadmin/layui/css/layui.css')}}" media="all">
+    <link rel="stylesheet" href="{{asset('static/admin/layuiadmin/style/admin.css')}}" media="all">
+</head>
+<body>
+<div class="layui-fluid">
+    @yield('content')
+</div>
+<script src="{{asset('static/admin/layuiadmin/layui/layui.js')}}"></script>
+<script>
+    layui.config({
+        base: '/static/admin/layuiadmin/' //静态资源所在路径
+    }).extend({
+        index: 'lib/index' //主入口模块
+    }).use('index');
+</script>
+@yield('script')
+</body>
+</html>
